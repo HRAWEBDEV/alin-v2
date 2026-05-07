0b786c7e499eed13990294d72f3021de01d22a19
HRAWEBDEV <hrawebdev@gmail.com>
2026-05-07 10:23:52 +0330

    add locale controller button

--- app/[lang]/(app)/auth/components/AuthControllers.tsx
+++ app/[lang]/(app)/auth/components/AuthControllers.tsx
@@ -1,9 +1,12 @@
 import { ModeControllerButton } from "../../components/ModeController";
+import { LocaleControllerButton } from "../../components/LocaleController";
+
 export default function AuthControllers() {
   return (
     <div className="flex gap-4 justify-between h-full items-center p-3 px-6">
       <div className="flex gap-4">
         <ModeControllerButton />
+        <LocaleControllerButton />
       </div>
     </div>
   );
